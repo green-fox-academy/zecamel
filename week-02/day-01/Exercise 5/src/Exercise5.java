49b1bb25d19a61022bc641b8b87e71c1e6861b16
the_camel <the_camel@icloud.com>
2017-11-27 16:15:49 +0100

    w2d1e1-7

---
+++ week-02/day-01/Exercise 5/src/Exercise5.java
@@ -0,0 +1,16 @@
+public class Exercise5 {
+    public static void main(String[] args) {
+
+        float a = 13.0f;
+        float b = 22.0f;
+
+        System.out.println(a + b);
+        System.out.println(b-a);
+        System.out.println(b*a);
+        System.out.println(b/a);
+        System.out.println((b/a)-((b/a) % 1));
+        System.out.println(b % a);
+
+
+    }
+}
