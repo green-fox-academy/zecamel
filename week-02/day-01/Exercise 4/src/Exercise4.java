aeb8fbd6d9cb2f01134d5dae5b00fe4903b7b2e9
the_camel <the_camel@icloud.com>
2017-11-27 16:34:21 +0100

    remove all, gitignore failed

--- week-02/day-01/Exercise 4/src/Exercise4.java
+++
@@ -1,7 +0,0 @@
-public class Exercise4 {
-    public static void main(String[] args) {
-        System.out.println("John Doe");
-        System.out.println("31");
-        System.out.println("1.87");
-    }
-}
